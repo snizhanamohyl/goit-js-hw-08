52e4e3c1b9250e2389e1f625a49ce9a120abfafb
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-01-16 19:47:05 +0200

    tasks 2,3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  iframePlayer.setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time'))
+  );
+}
+
+iframePlayer.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const submitBtn = document.querySelector('button[type=submit]');
+const formValue = { email: '', message: '' };
+
+const storageData = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (storageData) {
+  formEl.querySelector("input[name='email']").value = storageData.email;
+  formValue.email = storageData.email;
+  formEl.querySelector("textarea[name='message']").value = storageData.message;
+  formValue.message = storageData.message;
+}
+
+formEl.addEventListener(
+  'input',
+  throttle(event => {
+    formValue.email =
+      event.target.name === 'email' ? event.target.value : formValue.email;
+
+    formValue.message =
+      event.target.name === 'message' ? event.target.value : formValue.message;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formValue));
+  }, 500)
+);
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const submitFormValue = {
+    email: formEl.querySelector("input[name='email']").value,
+    message: formEl.querySelector("textarea[name='message']").value,
+  };
+
+  if (!submitFormValue.email || !submitFormValue.message) {
+    alert('Заповніть всі дані!');
+    return;
+  }
+
+  localStorage.clear();
+
+  formEl.reset();
+
+  console.log(submitFormValue);
+  return submitFormValue;
+}
